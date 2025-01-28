4186aaa191e9c6da722c89c47c2c1676d5b6f396
jpnarchi <142521385+jpnarchi@users.noreply.github.com>
2025-01-28 11:16:32 -0600

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,7 +5,7 @@ import { Chat } from '~/components/chat/Chat.client';
 import { Header } from '~/components/header/Header';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'Papayo' }, { name: 'description', content: 'Habla con Papayo, un agente de IA que crea tus ideas en proyectos.' }];
 };
 
 export const loader = () => json({});
